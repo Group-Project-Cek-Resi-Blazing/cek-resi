18935e29ce596161bbb4701b4b4d6b3f6ee2cf24
nugrahakiat <nugrahakiatsaputra@gmail.com>
2021-07-08 21:55:44 +0700

    Check Resi API

--- server/controllers/ResiController.js
+++ server/controllers/ResiController.js
@@ -1,16 +1,15 @@
+const axios = require('axios');
 class ResiController {
     static chekResi(req, res, next) {
-        axios.get('https://api.binderbyte.com/v1/track')
+        const {courier, awb} = req.body
+        axios.get(`https://api.binderbyte.com/v1/track?api_key=${process.env.api_key}&courier=${courier}&awb=${awb}`)
         .then(data => {
-            console.log(data)
-            res.status(200).JSON(data)
+            res.status(200).json(data.data.data)
         })
         .catch(error => {
             console.log(error)
             next(error)
         })
-
-
     }
 }
 
