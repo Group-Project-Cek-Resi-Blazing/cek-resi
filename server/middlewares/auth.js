e6bf27d2d7217f1edfd10fbd0b026877ddda56bb
ardiansyakh <abdullah.ard9@gmail.com>
2021-07-08 15:35:26 +0700

    auth, jwt

---
+++ server/middlewares/auth.js
@@ -0,0 +1,49 @@
+const {User, Resi} = require('../models')
+const {decode} = require('./jwt')
+
+
+const authentication = (req, res, next)=>{
+    try{
+        if(!req.headers.access_token){
+            throw({name: 'NOT_LOGIN'})
+        }
+        const access_token = req.headers
+        const {id} = decode(access_token)
+        const getUser = await User.findByPk(id)
+    
+        if(!getUser){
+            throw({name: 'AUTHENTICATION_FAILED'})
+        }
+    
+        req.user = getUser
+        next()
+    }
+    catch(err){
+        next(err)
+    }
+
+}
+
+const authorization = (req, res, next)=>{
+    const {id} = req.params
+    const userId = req.user.id
+    Resi.findOne({
+        where:{
+            id, 
+            userId
+        }
+    })
+    .then(result=>{
+        if(!result){
+            throw({name: 'RESI_NOT_FOUND'})
+        }
+        req.resi = result
+        next()
+    })
+    .catch(err=>{
+        next(err)
+    })
+
+}
+
+module.exports = {authentication, authorization}
--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,3 +1,25 @@
-const errorHandler = (err, req, res, next) => {}
+const errorHandler = (err, req, res, next) => {
+    let statusCode
+    let msg
+    switch(err){
+        case 'NOT_LOGIN':
+            statusCode = 401
+            msg = {message: 'Please Login First'}
+            break
+        case 'AUTHENTICATION_FAILED' :
+            statusCode= 401
+            msg = {message: 'Authentication Failed'}
+            break
+        case 'RESI_NOT_FOUND' :
+            statusCode= 404
+            msg = {message: 'Resi Not Found'}
+            break
+        default :
+            statusCode = 500
+            errorMessage = {message: err}
+            break
+
+    }
+}
 
 module.exports = errorHandler
