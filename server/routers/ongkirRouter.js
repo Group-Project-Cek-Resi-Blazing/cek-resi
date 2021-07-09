8b667632a258222aa28b2edb045861c3fdd39923
juandari <juandaari70@gmail.com>
2021-07-09 13:04:46 +0700

    add cities router

--- server/routers/ongkirRouter.js
+++ server/routers/ongkirRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const OngkirController = require('../controllers/OngkirController')
 
 router.post('/', OngkirController.getOngkir)
+router.get('/', OngkirController.getCities)
 
 module.exports = router
